61baabca0323d716f7d26d6b4a33511bf9959f3d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-10-05 15:23:02 -0500

    improve shadows

--- src/lights/index.js
+++ src/lights/index.js
@@ -8,11 +8,30 @@ export const directionalLight = new THREE.DirectionalLight(color, 1.5);
 directionalLight.position.set(3, 2, -8);
 directionalLight.castShadow = true;
 
+directionalLight.shadow.mapSize.width = 256;
+directionalLight.shadow.mapSize.height = 256;
+directionalLight.shadow.camera.top = 8;
+directionalLight.shadow.camera.right = 8;
+directionalLight.shadow.camera.bottom = -8;
+directionalLight.shadow.camera.left = -8;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 20;
 export const doorLight = new THREE.PointLight("#ff7d46", 5);
 
 export const ghostLight1 = new THREE.PointLight("#8800ff", 6);
 ghostLight1.castShadow = true;
+ghostLight1.shadow.mapSize.width = 256;
+ghostLight1.shadow.mapSize.height = 256;
+ghostLight1.shadow.camera.fat = 10;
+
 export const ghostLight2 = new THREE.PointLight("#ff0088", 6);
 ghostLight2.castShadow = true;
+ghostLight2.shadow.mapSize.width = 256;
+ghostLight2.shadow.mapSize.height = 256;
+ghostLight2.shadow.camera.fat = 10;
+
 export const ghostLight3 = new THREE.PointLight("#ff0000", 6);
 ghostLight3.castShadow = true;
+ghostLight3.shadow.mapSize.width = 256;
+ghostLight3.shadow.mapSize.height = 256;
+ghostLight3.shadow.camera.fat = 10;
