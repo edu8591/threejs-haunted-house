4b563f655d5d289e73596c95332baac81bb33774
Eduardo Serrano <eduardoserrano8591@gmail.com>
2024-10-05 15:31:51 -0500

    adds sky

--- src/script.js
+++ src/script.js
@@ -12,6 +12,7 @@ import {
   ghostLight2,
   ghostLight3,
 } from "./lights";
+import { Sky } from "three/addons/objects/Sky.js";
 
 const loadingManager = new THREE.LoadingManager();
 loadingManager.onError = (e) => {
@@ -38,6 +39,15 @@ scene.add(ghostLight1);
 scene.add(ghostLight2);
 scene.add(ghostLight3);
 
+const sky = new Sky();
+sky.material.uniforms.turbidity.value = 10;
+sky.material.uniforms.rayleigh.value = 3;
+sky.material.uniforms.mieCoefficient.value = 0.1;
+sky.material.uniforms.mieDirectionalG.value = 0.95;
+sky.material.uniforms.sunPosition.value.set(0.3, -0.038, -0.95);
+
+sky.scale.setScalar(100);
+scene.add(sky);
 /**
  * Sizes
  */
